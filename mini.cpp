cac829b02b83e1c66eabc825e0e9fc89dadc0e49
sajith kavishan <sajithkavishan1111@gmail.com>
2023-05-02 00:09:48 +0530

    third commit

--- mini.cpp
+++ mini.cpp
@@ -8,6 +8,8 @@ using namespace std;
 int main()
 {
     int count = 0;
+    int invalid_input = 0;
+
     do {
         string open;
         cout << "You have " << 3 - count << " Mystery Box. \n";
@@ -28,34 +30,36 @@ int main()
                     cout << "You win a Bumber sticker. \n \n";
                     break;
                 case 2:
-                    cout << "You win a lost dog. \n \n";
+                    cout << "You win a Play station 5. \n \n";
                     break;
                 case 3:
                     cout << "You win nothing. \n \n";
                     break;
                 case 4:
-                    cout << "You win a free lunch. \n \n";
+                    cout << "You win a x box. \n \n";
                     break;
                 case 5:
-                    cout << "You win a bat. \n \n";
+                    cout << "You win a free lunch. \n \n";
                     break;
                 case 6:
-                    cout << "You win a fish. \n \n";
+                    cout << "You win a cheap mouse. \n \n";
                     break;
                 case 7:
                     cout << "You win a vibrator. \n \n";
                     break;
                 case 8:
-                    cout << "You win a free chance. \n \n";
+                    cout << "You win a Mystery Box. \n \n";
+                    count--;
                     break;
                 case 9:
-                    cout << "You win a socks. \n \n";
+                    cout << "You win a fake rolex watch. \n \n";
                     break;
                 case 10:
-                    cout << "You win nothing and you suck. \n \n";
+                    cout << "You win nothing, you suck. \n \n";
                     break;
             }
             count++;
+            invalid_input = 0;
         }
         else if (open == "no") {
             cout << "You choose not to open the Mystery Box. \n \n";
@@ -63,7 +67,12 @@ int main()
         }
         else {
             cout << "Please enter yes or no. \n \n";
-            count = 0;
+            invalid_input++;
+        }
+
+        if(invalid_input == 3){
+            cout << "Too many invalid answers. please try again. \n \n";
+            break;
         }
         
     } while (count < 3);
