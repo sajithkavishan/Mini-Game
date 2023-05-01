bda9bca93c5e420853aa897d72a331da6f61cd05
sajith kavishan <sajithkavishan1111@gmail.com>
2023-05-01 22:32:57 +0530

    fisrt commit

--- mini.cpp
+++ mini.cpp
@@ -10,14 +10,14 @@ int main()
     int count = 0;
     do {
         string open;
-        cout << "You have 3 Mystery Box. ";
+        cout << "You have " << 3 - count << " Mystery Box. \n";
         cout << "Do you want to open it? ";
         cin >> open;
 
         for (int i = 0; i < open.length(); i++) {
             open[i] = tolower(open[i]);
         }
-        cout << open << endl;
+        // cout << open << endl;
 
         if (open == "yes") {
             srand(time(0));
@@ -25,44 +25,47 @@ int main()
 
             switch (randnum) {
                 case 1:
-                    cout << "You win a Bumber sticker. \n";
+                    cout << "You win a Bumber sticker. \n \n";
                     break;
                 case 2:
-                    cout << "You win a lost dog. \n";
+                    cout << "You win a lost dog. \n \n";
                     break;
                 case 3:
-                    cout << "You win nothing. \n";
+                    cout << "You win nothing. \n \n";
                     break;
                 case 4:
-                    cout << "You win a free lunch. \n";
+                    cout << "You win a free lunch. \n \n";
                     break;
                 case 5:
-                    cout << "You win a bat. \n";
+                    cout << "You win a bat. \n \n";
                     break;
                 case 6:
-                    cout << "You win a fish. \n";
+                    cout << "You win a fish. \n \n";
                     break;
                 case 7:
-                    cout << "You win a vibrator. \n";
+                    cout << "You win a vibrator. \n \n";
                     break;
                 case 8:
-                    cout << "You win a free chance. \n";
+                    cout << "You win a free chance. \n \n";
                     break;
                 case 9:
-                    cout << "You win a socks. \n";
+                    cout << "You win a socks. \n \n";
                     break;
                 case 10:
-                    cout << "You win nothing and you suck. \n";
+                    cout << "You win nothing and you suck. \n \n";
                     break;
             }
+            count++;
         }
         else if (open == "no") {
-            cout << "You chose not to open the Mystery Box. \n";
+            cout << "You choose not to open the Mystery Box. \n \n";
+            break;
         }
         else {
-            cout << "Please enter yes or no. \n";
+            cout << "Please enter yes or no. \n \n";
+            count = 0;
         }
-        count++;
+        
     } while (count < 3);
 
     return 0;
